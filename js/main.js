d502fe7ad549d124d35f1cb45f5f8417dccccd29
alaaelzayat <alaaelzayat93>
2019-07-27 10:31:03 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+/*global $,alert,console*/
+$(function(){
+  'use strict'
+  //owl-carousel
+  
+  $('.owl-carousel').owlCarousel({
+    
+   
+    
+   
+})
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementByclass("navbar").style.top = "0";
+  } else {
+    document.getElementByclass("navbar").style.top = "-50px";
+  }
+}*/
+/*slider*/
+ 
+
+
+
+
+
