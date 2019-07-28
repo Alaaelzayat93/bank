e369701d550eda1e63400ec1034e7b74e0762dc6
alaaelzayat <alaaelzayat93>
2019-07-28 06:27:11 -0700

    new

--- js/main.js
+++ js/main.js
@@ -1,17 +1,32 @@
 /*global $,alert,console*/
-$(function(){
-  'use strict'
+
   //owl-carousel
-  
-  $('.owl-carousel').owlCarousel({
-    
-   
-    
-   
-})
+$(document).ready(function () {
+    "use strict";
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        rtl: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
 });
+
+
+
+
 //dropdown
-$('.dropdown-toggle').dropdown()
+
 
 
 
